9e0c0a2e4d92c9b636fe030378a31754e8eca7c2
Jonathan Rojas <jonathansw01@gmail.com>
2023-08-09 23:44:38 -0400

    Ajustes home

--- src/app/pages/nosotros/nosotros.component.ts
+++ src/app/pages/nosotros/nosotros.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component    } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BannerAboutComponent } from 'src/app/sections/banner-about/banner-about.component';
+
+
+
 @Component({
-  selector: 'app-nosotros',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './nosotros.component.html',
-  styleUrls: ['./nosotros.component.scss']
+  	selector: 'app-nosotros',
+  	standalone: true,
+  	imports: [CommonModule, BannerAboutComponent],
+  	templateUrl: './nosotros.component.html',
+  	styleUrls: ['./nosotros.component.scss']
 })
+
+
+
 export class NosotrosComponent {
+
+
 
 }
---
+++ src/app/sections/banner-about/banner-about.component.ts
@@ -0,0 +1,20 @@
+import { Component    } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@Component({
+  	selector: 'app-banner-about',
+ 	standalone: true,
+  	imports: [CommonModule],
+  	templateUrl: './banner-about.component.html',
+  	styleUrls: ['./banner-about.component.scss']
+})
+
+
+
+export class BannerAboutComponent {
+
+	
+
+}
