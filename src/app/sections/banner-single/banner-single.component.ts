8e88a877efd2f4806441872e7f482cce41603826
Jonathan Rojas <jonathansw01@gmail.com>
2023-08-16 12:14:23 -0400

    Ajustes del modal

--- src/app/sections/banner-single/banner-single.component.ts
+++ src/app/sections/banner-single/banner-single.component.ts
@@ -4,13 +4,14 @@ import { CommonModule     } from '@angular/common';
 import { Producto } from 'src/app/interfaces/producto.interface';
 
 import { DefaultImagePipe } from 'src/app/shared/pipes/default-image.pipe';
+import { ModalComponent } from '../../shared/modal/modal.component';
 
 
 
 @Component({
   	selector: 'banner-single',
   	standalone: true,
- 	imports: [CommonModule, DefaultImagePipe],
+ 	imports: [CommonModule, DefaultImagePipe, ModalComponent],
   	templateUrl: './banner-single.component.html',
   	styleUrls: ['./banner-single.component.scss']
 })
--- src/app/sections/contenido/contenido.component.ts
+++ src/app/sections/contenido/contenido.component.ts
@@ -1,12 +1,14 @@
 import { Component    } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
+
 
 
 @Component({
   	selector: 'contenido',
   	standalone: true,
-  	imports: [CommonModule],
+  	imports: [CommonModule, ModalComponent],
   	templateUrl: './contenido.component.html',
   	styleUrls: ['./contenido.component.scss']
 })
