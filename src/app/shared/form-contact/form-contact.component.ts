889dd8acf3000a79e9c8a059ac926ee623c81920
Jonathan Rojas <jonathansw01@gmail.com>
2023-08-17 15:39:17 -0400

    ajustes en formulario

--- src/app/shared/form-contact/form-contact.component.ts
+++ src/app/shared/form-contact/form-contact.component.ts
@@ -4,6 +4,8 @@ import { CommonModule     } from '@angular/common';
 import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder ,FormGroup, Validators } from '@angular/forms';
 
+import Swal from 'sweetalert2';
+
 
 
 @Component({
@@ -33,7 +35,7 @@ export class FormContactComponent {
 		marca:     [ '' ],
 		model:     [ '' ],
 		year:      [ '' ],
-		patente:   [ '', [Validators.required] ],
+		patente:   [ '', [Validators.required, this.patenteValidator ]],
 		cuota:     [ '' ],
 		deducible: [ '' ]
 	});
@@ -118,22 +120,93 @@ export class FormContactComponent {
 	}
 
 
-	onSave(): void {
+	formatPatente(event: any): void {
+		
+		const input      = event.target as HTMLInputElement;
+		let patenteValue = input.value.replace(/[^A-Za-z0-9]/g, ''); // Permitir solo letras y números
+		patenteValue     = patenteValue.slice(0, 6).toUpperCase(); // Limitar a 6 caracteres y convertir a mayúsculas
+	  
+		if ( patenteValue.length > 2 ) {
+		  	
+			const firstPart  = patenteValue.slice(0, 2);
+		  	const secondPart = patenteValue.slice(2, 4);
+		  	const thirdPart  = patenteValue.slice(4);
+		  	patenteValue     = `${firstPart}-${secondPart}-${thirdPart}`;
 
+		} else if ( patenteValue.length > 1 ) {
+
+		  	const firstPart  = patenteValue.slice(0, 2);
+		  	const secondPart = patenteValue.slice(2);
+		  	patenteValue     = `${firstPart}-${secondPart}`;
+		
+		}
+	  
+		input.value = patenteValue;
+		this.myFormCar.get('patente')?.setValue(patenteValue, { emitEvent: false });
+	}
+	
+
+	private patenteValidator(control: AbstractControl): { [key: string]: boolean } | null {
+		
+		const value = control.value;
+		
+		if (!value) {
+		  	return null; // No mostrar error si el campo está vacío
+		}
+	  
+		// Expresión regular para validar patente chilena (formato: XXXX-XX)
+		const patentePattern = /^[A-Z0-9]{4}-[A-Z0-9]{2}$/;
+	  
+		if (!patentePattern.test(value)) {
+		  	return { invalidPatente: true }; // Mostrar error si el formato no es válido
+		}
+	  
+		// Si pasa todas las validaciones, retorna null
+		return null;
+	
+	}
+	
+
+	onSave(): void {
+		
 		if ( this.isCar ) {
 			console.log( 'Form CAR' );
-			if ( this.myFormCar.invalid ) return;
+			if ( this.myFormCar.invalid ) {
+				Swal.fire({
+					icon: 'error',
+					text: 'Por favor, completa todos los campos obligatorios.'
+				});
+				return;
+			}
 			console.log( this.myFormCar.value );
+			
+			Swal.fire({
+				icon: 'success',
+				text: 'Tu información se ha enviado con éxito a nuestros ejecutivos, y te contactaremos lo más pronto posible, para que tu experiencia sea FASIL.'
+			});
+
 			this.myFormCar.reset();
 		}
 
 		if ( this.isGeneral ) {
 			console.log( 'Form GENERAL' );
-			if ( this.myFormGeneral.invalid ) return;
+			if ( this.myFormGeneral.invalid ) {
+				Swal.fire({
+					icon: 'error',
+					text: 'Por favor, completa todos los campos obligatorios.'
+				});
+				return;
+			};
 			console.log( this.myFormGeneral.value );
+
+			Swal.fire({
+				icon: 'success',
+				text: 'Tu información se ha enviado con éxito a nuestros ejecutivos, y te contactaremos lo más pronto posible, para que tu experiencia sea FASIL.'
+			});
+
 			this.myFormGeneral.reset();
 		}
-		
+
 	}
 
 
